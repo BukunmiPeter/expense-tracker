b045729daae5df92af059c55241c04fba0f4b93c
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-01-24 09:58:41 +0100

    ...

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -24,6 +24,7 @@ function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
